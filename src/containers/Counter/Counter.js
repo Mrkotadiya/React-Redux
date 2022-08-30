c6d67f6529f059767541c220599a9575c5e58176
nirbhay <nirbhayk7281@gmail.com>
2022-08-30 10:43:14 +0530

    updating state immulty

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -6,7 +6,8 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 class Counter extends Component {
     state = {
-        counter: 0
+        counter: 0,
+        // results:[]
     }
 
     counterChangedHandler = ( action, value ) => {
@@ -36,6 +37,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={() => this.props.onDecrementCounter()}  />
                 <CounterControl label="Add 10" clicked={() => this.props.onAddCounter()}  />
                 <CounterControl label="Subtract 15" clicked={() => this.props.onSubtractCounter()}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}> Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li key={strResult.id } onClick={this.props.onDeleteResult} >{strResult.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -43,7 +51,8 @@ class Counter extends Component {
 
 const mapStateToProps = state =>{
     return {
-        ctr:state.counter
+        ctr:state.counter,
+        storedResults:state.results     
     }
 }
 
@@ -52,7 +61,9 @@ const mapDispatchToProps= dispatch =>{
             onIncrementCounter : ()=> dispatch({type:"INCREMENT"}),
             onDecrementCounter : ()=> dispatch({type:"DECREMENT"}),
             onAddCounter : ()=> dispatch({type:"ADD", val:10}),
-            onSubtractCounter : ()=> dispatch({type:"SUBTRACT", val:15})
+            onSubtractCounter : ()=> dispatch({type:"SUBTRACT", val:15}),
+            onStoreResult:()=> dispatch({type:"STORE_RESULT"}),
+            onDeleteResult:()=> dispatch({type:"DELETE_RESULT"})
         }
 }
 
