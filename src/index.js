8514dce20227739d32660916ad7c4a9f852f06ff
nirbhay <nirbhayk7281@gmail.com>
2022-09-15 09:49:36 +0530

    synchronous action creators

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore ,combineReducers} from "redux";
+import {createStore ,combineReducers ,applyMiddleware , compose} from "redux";
 // import {Provider} from 'react-redux';
 import { Provider } from 'react-redux';
 
@@ -13,10 +13,23 @@ import resultReducer from './store/reducers/results';
 const rootReducer = combineReducers({
     ctr: counterReducer,
     res:resultReducer
-})
+});
 
 
-const store = createStore(rootReducer);
+const logger = store =>{
+    return next => {
+        return action =>{
+            console.log('[Middleware] Dispatching', action);
+            const result=next(action);
+            console.log('[Middlewere] next state',store.getState());
+            return result;
+        }
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION__|| compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
