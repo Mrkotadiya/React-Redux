e35e0000810c83dbe9d3e3eac911b644c4b79db1
nirbhay <nirbhayk7281@gmail.com>
2022-09-15 17:23:35 +0530

    using action create & get state and refactoring reducers utility func

--- src/store/reducers/results.js
+++ src/store/reducers/results.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility';
 
 const initialState = {
     results:[]
@@ -8,20 +9,17 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
       //change Data
-      return {
-        ...state,
-        results:state.results.concat({id:new Date(),value:action.result * 2})
-      };
-      case actionTypes.DELETE_RESULT:
+      return updateObject(state,{ results:state.results.concat({id:new Date(),value:action.result * 2})})
+
+
+    case actionTypes.DELETE_RESULT:
         // const id= 2;
         // const newArray = [...state.results];
         // newArray.results.splice(id,1)
         const updatedArray = state.results.filter(result=> result.id !== action.resultElId);
-        return {
-          ...state,
-          results:updatedArray
-        };
-    default :
+        return updateObject(state,{results:updatedArray}); 
+
+        default :
   }
   return state;
 };
